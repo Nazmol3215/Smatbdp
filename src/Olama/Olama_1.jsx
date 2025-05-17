b4aa76cb3a02b540f0d3bf56c182cd6939752c57
Nazmol3215 <mdasikahamad2006@gmail.com>
2025-05-17 12:25:36 +0600

    first commit

---
+++ src/Olama/Olama_1.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import OffcanvasExample from "../Layout/OffcanvasExample";
+import Footer from "../Layout/Footer";
+import Olama from "./Olama";
+
+export default function Olama_1() {
+  return (
+    <div>
+
+<OffcanvasExample/>
+<Olama/>
+<Footer/>
+    </div>
+  )
+}
